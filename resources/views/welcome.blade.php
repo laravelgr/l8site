1769e81c7f4008d38b5f4e2d208d822b3fe4b623
octoper <me@octoper.me>
2020-11-21 21:26:15 +0100

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -118,14 +118,16 @@
                             <x-heroicon-o-mail class="w-8 h-8 -mt-1 text-gray-500"/>
                             <div class="ml-4 text-lg leading-7 font-semibold w-full">
                                 <p class="text-gray-800">Subscribe to our Newsletter!</p>
-                                <div class="mt-4 relative lg:w-2/3">
-                                    <form class="flex xl:flex-row flex-col">
-                                        <input type="email" name="email" placeholder="your@emai.com" required=""
-                                               class="rounded-sm ring-2 ring-blue-200 focus:ring-blue-500 outline-none
-                                               text-black flex-grow px-4 py-2" autocomplete="off">
+                                <div class="mt-3 relative lg:w-2/3">
+                                    <label for="email" class="text-gray-600">Email</label>
+                                    <form class="flex mt-1 xl:flex-row flex-col">
+                                            <input id="email" type="email" name="email" placeholder="your@emai.com" required=""
+                                                   class="rounded-sm ring-2 ring-gray-300 focus:ring-blue-500 outline-none
+                                                   text-black flex-grow px-4 py-2" autocomplete="off">
+
                                         <button class="text-white focus:ring-offset-2 focus:ring-offset-2
                                             focus:ring focus:ring-blue-400 focus:outline-none rounded-sm bg-blue-500
-                                            mt-4 xl:mt-0 xl:ml-4 shadow-md px-6 py-3">Subscribe</button>
+                                            mt-4 xl:mt-0 xl:ml-4 shadow-md px-6 py-2">Subscribe</button>
                                     </form>
                                 </div>
                             </div>
